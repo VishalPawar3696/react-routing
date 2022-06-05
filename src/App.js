7077c80c4cafea1e396f7f43fc2238cd83cb63f5
VishalPawar3696 <vishalpawar3696@gmail.com>
2022-06-05 19:13:58 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import Products from './components/Products';
+import {Route,Routes} from "react-router-dom";
+import About from './components/About';
+import Home from './components/Home';
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+     <Routes>
+       <Route Path="/Home" element={<Home/>}/>
+       <Route Path="/About" element={<About/>}/>
+       <Route Path="/Product" element={<Products/>}/>
+
+
+       
+     </Routes>
     </div>
+
   );
 }
 
